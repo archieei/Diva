6e840b2bfa7e630805cab747ea19aa28d6392901
archieei <143136068+archieei@users.noreply.github.com>
2026-04-12 20:04:39 +1000

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "✨She is serving rn💅",
+        name: "✨She is serving rn✨💅",
         // Activity type number (0 = Playing).
         type: 4, 
       },
